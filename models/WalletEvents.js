c5f64e3173fb52d8e69f8a28ca1022549554bb0d
Vladimir <vzangi@yandex.ru>
2023-09-25 14:54:16 +0300

    fix walletevents async calls

--- models/WalletEvents.js
+++ models/WalletEvents.js
@@ -5,44 +5,41 @@ const Account = require('./Account')
 const { getCoolDateTime } = require('../units/helpers')
 
 const events = {
-  PAYMENT: 0,   // Пополнение
-  MARRIAGE: 1,  // Свадьба
-  RECALL: 2,    // Отзыв предложения
-  DIVORCE: 3,   // Развод
-  SELLING: 4,   // Продажа
-  BUY: 5,       // Покупка  
+  PAYMENT: 0, // Пополнение
+  MARRIAGE: 1, // Свадьба
+  RECALL: 2, // Отзыв предложения
+  DIVORCE: 3, // Развод
+  SELLING: 4, // Продажа
+  BUY: 5, // Покупка
 }
 
-const WalletEvent = sequelize.define(
-  'walletevents',
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+const WalletEvent = sequelize.define('walletevents', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  accountId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  eventType: {
+    type: DataTypes.INTEGER,
+  },
+  value: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  thingId: {
+    type: DataTypes.INTEGER,
+  },
+  coolDate: {
+    type: DataTypes.VIRTUAL(DataTypes.STRING),
+    get() {
+      return getCoolDateTime(this.createdAt)
     },
-    accountId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    eventType: {
-      type: DataTypes.INTEGER,
-    },
-    value: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    thingId: {
-      type: DataTypes.INTEGER,
-    },
-    coolDate: {
-      type: DataTypes.VIRTUAL(DataTypes.STRING),
-      get() {
-        return getCoolDateTime(this.createdAt)
-      }
-    }
-  }
-)
+  },
+})
 
 WalletEvent.events = events
 
@@ -58,21 +55,22 @@ WalletEvent.belongsTo(Account)
 const transaction = async (eventType, accountId, value) => {
   const t = await sequelize.transaction()
   try {
-
-    await WalletEvent.create({
-      accountId,
-      eventType,
-      value,
-    }, { transaction: t })
+    await WalletEvent.create(
+      {
+        accountId,
+        eventType,
+        value,
+      },
+      { transaction: t }
+    )
 
     await Account.increment('wallet', {
       by: value,
       where: { id: accountId },
-      transaction: t
+      transaction: t,
     })
 
     await t.commit()
-
   } catch (error) {
     await t.rollback()
     throw new Error(error)
@@ -81,22 +79,30 @@ const transaction = async (eventType, accountId, value) => {
 
 // Транзакция пополнения кошелька
 WalletEvent.payment = async (userId, sum) => {
-  transaction(WalletEvent.events.PAYMENT, userId, sum)
+  await transaction(WalletEvent.events.PAYMENT, userId, sum)
 }
 
 // Транзакция предложения пойти в ЗАГС
 WalletEvent.marriage = async (userId) => {
-  transaction(WalletEvent.events.MARRIAGE, userId, -WalletEvent.marriageCost)
+  await transaction(
+    WalletEvent.events.MARRIAGE,
+    userId,
+    -WalletEvent.marriageCost
+  )
 }
 
 // Транзакция отзыва предложения
 WalletEvent.recall = async (userId) => {
-  transaction(WalletEvent.events.RECALL, userId, WalletEvent.recallCost)
+  await transaction(WalletEvent.events.RECALL, userId, WalletEvent.recallCost)
 }
 
 // Транзакция развода
 WalletEvent.divorce = async (userId) => {
-  transaction(WalletEvent.events.DIVORCE, userId, -WalletEvent.divorceCost)
+  await transaction(
+    WalletEvent.events.DIVORCE,
+    userId,
+    -WalletEvent.divorceCost
+  )
 }
 
 module.exports = WalletEvent
