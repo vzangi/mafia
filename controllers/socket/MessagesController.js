31f0224de3668621cf0f80cf0a20beef20388e1d
Vladimir <vzangi@yandex.ru>
2023-09-26 17:32:23 +0300

    private messages stage 2

--- controllers/socket/MessagesController.js
+++ controllers/socket/MessagesController.js
@@ -1,10 +1,5 @@
-const { Op } = require('sequelize')
 const BaseSocketController = require('./BaseSocketController')
 const Message = require('../../models/Message')
-const Friend = require('../../models/Friend')
-const Account = require('../../models/Account')
-const sequelize = require('../../units/db')
-const limit = 10
 
 class MessagesController extends BaseSocketController {
   // Проверка на дружеские отношения
@@ -12,21 +7,7 @@ class MessagesController extends BaseSocketController {
     const { user } = this
     if (!user) return callback({ status: 2, msg: 'Неавторизованный запрос' })
 
-    const relations = await Friend.findOne({
-      where: {
-        accountId: user.id,
-        friendId,
-        [Op.or]: [
-          { status: Friend.statuses.MARRIED },
-          { status: Friend.statuses.ACCEPTED },
-        ],
-      },
-      include: {
-        model: Account,
-        as: 'friend',
-        attributes: ['id', 'avatar', 'vipTo', 'vip', 'online', 'username'],
-      },
-    })
+    const relations = await Message.canMassaging(user.id, friendId)
 
     if (!relations)
       return callback({
@@ -42,61 +23,17 @@ class MessagesController extends BaseSocketController {
     const { user } = this
     if (!user) return callback({ status: 2, msg: 'Неавторизованный запрос' })
 
-    const messages = await Message.findAll({
-      where: {
-        [Op.or]: [
-          {
-            [Op.and]: [{ accountId: user.id }, { friendId }],
-          },
-          {
-            [Op.and]: [{ accountId: friendId }, { friendId: user.id }],
-          },
-        ],
-      },
-      offset,
-      limit,
-    })
+    const messages = await Message.getPrivateMessages(user.id, friendId, offset)
 
     callback({ status: 0, messages })
   }
 
+  // Получение списка друзей с которыми есть приватный чат
   async getList(callback) {
     const { user } = this
     if (!user) return callback({ status: 2, msg: 'Неавторизованный запрос' })
 
-    const [results, _] = await sequelize.query(`
-    SELECT m1.id
-    FROM messages as m1
-    where 
-        m1.id = (SELECT max(m2.id) FROM messages as m2 
-                WHERE 
-                    (m2.accountId = m1.accountId and m2.friendId = m1.friendId)
-                    or
-                    (m2.accountId = m1.friendId and m2.friendId = m1.accountId)
-                )
-        and
-        (m1.accountId = ${user.id} OR m1.friendId = ${user.id});`)
-
-    const lastMsgs = await Message.findAll({
-      where: {
-        id: results.map((r) => r.id),
-      },
-      // raw: true,
-      attributes: ['id', 'friendId', 'accountId', 'message'],
-      order: [['id', 'DESC']],
-      include: [
-        {
-          model: Account,
-          as: 'friend',
-          attributes: ['id', 'avatar', 'vipTo', 'vip', 'online', 'username'],
-        },
-        {
-          model: Account,
-          as: 'account',
-          attributes: ['id', 'avatar', 'vipTo', 'vip', 'online', 'username'],
-        },
-      ],
-    })
+    const lastMsgs = await Message.lastMessages(user.id)
 
     callback({ status: 0, lastMsgs, userId: user.id })
   }
--- models/Message.js
+++ models/Message.js
@@ -1,6 +1,8 @@
-const { DataTypes } = require('sequelize')
+const { DataTypes, Op } = require('sequelize')
 const sequelize = require('../units/db')
 const Account = require('./Account')
+const Friend = require('./Friend')
+const limit = 10
 
 const Message = sequelize.define('messages', {
   id: {
@@ -28,5 +30,82 @@ const Message = sequelize.define('messages', {
 
 Message.belongsTo(Account, { as: 'account', foreignKey: 'accountId' })
 Message.belongsTo(Account, { as: 'friend', foreignKey: 'friendId' })
+
+Message.lastMessages = async (userId) => {
+  const [results, _] = await sequelize.query(`
+    SELECT m0.id, m0.accountId, m0.friendId, m0.message
+    FROM messages as m0
+    JOIN friends AS fr 
+      ON (fr.accountId = m0.accountId and fr.friendId = m0.friendId) and 
+          (fr.status = ${Friend.statuses.ACCEPTED} OR fr.status = ${Friend.statuses.MARRIED})
+    WHERE m0.id in (
+    SELECT m1.id
+    FROM messages as m1
+    where 
+      m1.id = (SELECT max(m2.id) FROM messages as m2 
+          WHERE 
+            (m2.accountId = m1.accountId and m2.friendId = m1.friendId)
+                    or
+                    (m2.accountId = m1.friendId and m2.friendId = m1.accountId)
+                )
+      and
+      (m1.accountId = ${userId} OR m1.friendId = ${userId}))`)
+
+  return await Message.findAll({
+    where: {
+      id: results.map((r) => r.id),
+    },
+    attributes: ['id', 'friendId', 'accountId', 'message'],
+    order: [['id', 'DESC']],
+    include: [
+      {
+        model: Account,
+        as: 'friend',
+        attributes: ['id', 'avatar', 'vipTo', 'vip', 'online', 'username'],
+      },
+      {
+        model: Account,
+        as: 'account',
+        attributes: ['id', 'avatar', 'vipTo', 'vip', 'online', 'username'],
+      },
+    ],
+  })
+}
+
+Message.canMassaging = async (accountId, friendId) => {
+  return await Friend.findOne({
+    where: {
+      accountId,
+      friendId,
+      [Op.or]: [
+        { status: Friend.statuses.MARRIED },
+        { status: Friend.statuses.ACCEPTED },
+      ],
+    },
+    include: {
+      model: Account,
+      as: 'friend',
+      attributes: ['id', 'avatar', 'vipTo', 'vip', 'online', 'username'],
+    },
+  })
+}
+
+Message.getPrivateMessages = async (accountId, friendId, offset) => {
+  return await Message.findAll({
+    where: {
+      [Op.or]: [
+        {
+          [Op.and]: [{ accountId }, { friendId }],
+        },
+        {
+          [Op.and]: [{ accountId: friendId }, { friendId: accountId }],
+        },
+      ],
+    },
+    offset,
+    limit,
+    order: [['id']],
+  })
+}
 
 module.exports = Message
