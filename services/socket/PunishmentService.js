444c0a6e427b60e8acad3d9afa7f27c536f29054
Vladimir <vzangi@yandex.ru>
2024-02-26 17:23:44 +0300

    removePunish message

--- services/socket/PunishmentService.js
+++ services/socket/PunishmentService.js
@@ -1,3 +1,5 @@
+const Chat = require('../../models/Chat')
+const Account = require('../../models/Account')
 const Punishment = require('../../models/Punishment')
 const BaseService = require('./BaseService')
 
@@ -8,11 +10,25 @@ class PunishmentService extends BaseService {
       throw new Error('Нет необходимых данных')
     }
 
-    const punish = await Punishment.findByPk(id)
+    const punish = await Punishment.findOne({
+      where: { id },
+      include: [
+        {
+          model: Account,
+        },
+      ],
+    })
 
     if (!punish) {
       throw new Error('Запрет не найден')
     }
+
+    const message = `Администратор отменил молчанку игрока [${punish.account.username}]`
+
+    const sysmsg = await Chat.sysMessage(message)
+
+    // Рассылаю сообщение всем подключенным пользователям
+    this.io.of('/lobbi').emit('chat.message', sysmsg)
 
     await punish.destroy()
   }
