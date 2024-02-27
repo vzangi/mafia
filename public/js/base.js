5ecad827480deb213cae0e56f732aaf6e3f14076
Vladimir <vzangi@yandex.ru>
2024-02-27 22:17:05 +0300

    all punishments

--- public/js/base.js
+++ public/js/base.js
@@ -1,83 +1,88 @@
 // Общий веб-сокет
 const socket = io()
 
+socket.on('logout', () => {
+	$.post('/logout')
+	setTimeout(() => location.reload(), 100)
+})
+
 // Звук нотификации
 let zvuk = new Audio('/sounds/notification.mp3')
 $('body').append(zvuk)
 
 // Воспроизводит звук на странице
 function playSound(sound) {
-  setTimeout(function () {
-    if (sound.readyState == 4) {
-      sound.play()
-    } else {
-      playSound(sound)
-    }
-  }, 10)
+	setTimeout(function () {
+		if (sound.readyState == 4) {
+			sound.play()
+		} else {
+			playSound(sound)
+		}
+	}, 10)
 }
 
 // Отображает нотификацию
 function notify(msg, timeout = 0, lvl = 0) {
-  lvls = ['success', 'warning', 'danger', 'info']
-  notifier.show(msg, '', lvls[lvl], '', timeout)
+	lvls = ['success', 'warning', 'danger', 'info']
+	notifier.show(msg, '', lvls[lvl], '', timeout)
 }
 
 function coolDate(date) {
-  const _ = (r) => (r > 9 ? r : `0${r}`)
+	const _ = (r) => (r > 9 ? r : `0${r}`)
 
-  const monthes = [
-    'янв',
-    'фев',
-    'мар',
-    'апр',
-    'мая',
-    'июн',
-    'июл',
-    'авг',
-    'сен',
-    'окт',
-    'ноя',
-    'дек',
-  ]
+	const monthes = [
+		'янв',
+		'фев',
+		'мар',
+		'апр',
+		'мая',
+		'июн',
+		'июл',
+		'авг',
+		'сен',
+		'окт',
+		'ноя',
+		'дек',
+	]
 
-  const d = new Date(date)
-  const day = d.getDate()
-  const month = monthes[d.getMonth()]
-  const year = d.getFullYear()
-  const hour = _(d.getHours())
-  const min = _(d.getMinutes())
+	const d = new Date(date)
+	const day = d.getDate()
+	const month = monthes[d.getMonth()]
+	const year = d.getFullYear()
+	const hour = _(d.getHours())
+	const min = _(d.getMinutes())
 
-  return `${day} ${month}  ${year} ${hour}:${min}`
+	return `${day} ${month}  ${year} ${hour}:${min}`
 }
 
 // Включаем тултипы
 function activateBSTooltips() {
-  const tooltipTriggerList = [].slice.call(
-    document.querySelectorAll('[data-bs-toggle="tooltip"]')
-  )
-  tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
+	const tooltipTriggerList = [].slice.call(
+		document.querySelectorAll('[data-bs-toggle="tooltip"]')
+	)
+	tooltipTriggerList.map(function (tooltipTriggerEl) {
+		return new bootstrap.Tooltip(tooltipTriggerEl)
+	})
 }
 
 $(function () {
-  activateBSTooltips()
+	activateBSTooltips()
 
-  // Действие по нажатию на кнопку
-  async function action(btn) {
-    const { id, question, event } = $(btn).data()
-    if (await confirm(question)) {
-      socket.emit(event, id, async (res) => {
-        if (res && res.status != 0) {
-          return await alert(res.msg)
-        }
-        location.reload()
-      })
-    }
-  }
+	// Действие по нажатию на кнопку
+	async function action(btn) {
+		const { id, question, event } = $(btn).data()
+		if (await confirm(question)) {
+			socket.emit(event, id, async (res) => {
+				if (res && res.status != 0) {
+					return await alert(res.msg)
+				}
+				location.reload()
+			})
+		}
+	}
 
-  // Кнопки с действиями
-  $('.action-btn').click(function () {
-    action(this)
-  })
+	// Кнопки с действиями
+	$('.action-btn').click(function () {
+		action(this)
+	})
 })
--- services/AuthService.js
+++ services/AuthService.js
@@ -4,159 +4,181 @@ const { mail } = require('../units/mailer')
 const { createToken } = require('../units/jwt')
 const Account = require('../models/Account')
 const AccountName = require('../models/AccountName')
+const Punishment = require('../models/Punishment')
+const { Op } = require('sequelize')
+const { getCoolDateTime } = require('../units/helpers')
 const saltNumber = 10
 
 class AuthService {
-  // Регистрация
-  async register(username, email, password, passwordConfirm, accept) {
-    const checkEmail = await Account.findOne({ where: { email } })
+	// Регистрация
+	async register(username, email, password, passwordConfirm, accept) {
+		const checkEmail = await Account.findOne({ where: { email } })
 
-    if (checkEmail) {
-      throw new Error('Email уже используется')
-    }
+		if (checkEmail) {
+			throw new Error('Email уже используется')
+		}
 
-    if (password !== passwordConfirm) {
-      throw new Error('Пароли не совпадают')
-    }
+		if (password !== passwordConfirm) {
+			throw new Error('Пароли не совпадают')
+		}
 
-    const checkNik = await Account.findOne({ where: { username } })
+		const checkNik = await Account.findOne({ where: { username } })
 
-    if (checkNik) {
-      throw new Error('Ник уже используется')
-    }
+		if (checkNik) {
+			throw new Error('Ник уже используется')
+		}
 
-    const secondCheck = await AccountName.findOne({ where: { username } })
+		const secondCheck = await AccountName.findOne({ where: { username } })
 
-    if (secondCheck) {
-      throw new Error('Ник занят')
-    }
+		if (secondCheck) {
+			throw new Error('Ник занят')
+		}
 
-    if (accept != 1) {
-      throw new Error('Необходимо согласиться с правилами сайта')
-    }
+		if (accept != 1) {
+			throw new Error('Необходимо согласиться с правилами сайта')
+		}
 
-    const hashedPassword = await hash(password, saltNumber)
+		const hashedPassword = await hash(password, saltNumber)
 
-    try {
-      await Account.create({
-        email,
-        username,
-        password: hashedPassword,
-      })
+		try {
+			await Account.create({
+				email,
+				username,
+				password: hashedPassword,
+			})
 
-      this._welcomeEmail(email, username)
-    } catch (error) {
-      console.log(error)
-      throw new Error('Ошибка при регистрации')
-    }
-  }
+			this._welcomeEmail(email, username)
+		} catch (error) {
+			console.log(error)
+			throw new Error('Ошибка при регистрации')
+		}
+	}
 
-  // Авторизация
-  async login(username, password) {
-    const user = await Account.findOne({ where: { username } })
+	// Авторизация
+	async login(username, password) {
+		const user = await Account.findOne({ where: { username } })
 
-    if (!user) {
-      throw new Error('Пользователь с таким ником не найден')
-    }
+		if (!user) {
+			throw new Error('Пользователь с таким ником не найден')
+		}
 
-    const match = await compare(password, user.password)
+		const match = await compare(password, user.password)
 
-    if (!match) {
-      throw new Error('Неверное сочетание логина и пароля')
-    }
+		if (!match) {
+			throw new Error('Неверное сочетание логина и пароля')
+		}
 
-    if (user.status == 0) {
-      throw new Error('Необходимо подтвердить почту')
-    }
+		if (user.status == 0) {
+			throw new Error('Необходимо подтвердить почту')
+		}
 
-    const accessToken = createToken(user)
+		// Проверяю нет ли запрета на вход
+		const hasNoLogin = await Punishment.findOne({
+			where: {
+				accountId: user.id,
+				type: Punishment.types.NO_LOGIN,
+				untilAt: {
+					[Op.gte]: new Date().toISOString(),
+				},
+			},
+		})
 
-    return accessToken
-  }
+		if (hasNoLogin) {
+			throw new Error(
+				`Вам запрещено входить на сайт до ${getCoolDateTime(
+					hasNoLogin.untilAt
+				)}`
+			)
+		}
 
-  // Подтверждение регистрации
-  async accept(email, hash) {
-    const match = await compare(email + process.env.SECRET_JWT_KEY, hash)
+		const accessToken = createToken(user)
 
-    if (!match) {
-      throw new Error('Токен не верный')
-    }
+		return accessToken
+	}
 
-    const user = await Account.findOne({
-      where: {
-        email: email,
-        status: 0,
-      },
-    })
+	// Подтверждение регистрации
+	async accept(email, hash) {
+		const match = await compare(email + process.env.SECRET_JWT_KEY, hash)
 
-    if (!user) {
-      throw new Error('Пользователь не найден')
-    }
+		if (!match) {
+			throw new Error('Токен не верный')
+		}
 
-    user.update({ status: 1 })
+		const user = await Account.findOne({
+			where: {
+				email: email,
+				status: 0,
+			},
+		})
 
-    const accessToken = createToken(user)
-    return accessToken
-  }
+		if (!user) {
+			throw new Error('Пользователь не найден')
+		}
 
-  // Запрос на восстановление пароля
-  async restore(email) {
-    const user = await Account.findOne({ where: { email } })
+		user.update({ status: 1 })
 
-    if (!user) {
-      throw new Error('Пользователь с таким email не найден')
-    }
+		const accessToken = createToken(user)
+		return accessToken
+	}
 
-    if (user.status == 0) {
-      throw new Error('Аккаунт привязанный к этому email не подтверждён')
-    }
+	// Запрос на восстановление пароля
+	async restore(email) {
+		const user = await Account.findOne({ where: { email } })
 
-    this._restoreEmail(user)
-  }
+		if (!user) {
+			throw new Error('Пользователь с таким email не найден')
+		}
 
-  // Авторизация по ссылке отправленной на почту для смены пароля
-  async restorePassword(email, restoreHash) {
-    const account = await Account.findOne({ where: { email } })
+		if (user.status == 0) {
+			throw new Error('Аккаунт привязанный к этому email не подтверждён')
+		}
 
-    if (!account) {
-      throw new Error('Пользователь с таким Email не найден')
-    }
+		this._restoreEmail(user)
+	}
 
-    const match = await compare(
-      account.email + account.password + process.env.SECRET_JWT_KEY,
-      restoreHash
-    )
+	// Авторизация по ссылке отправленной на почту для смены пароля
+	async restorePassword(email, restoreHash) {
+		const account = await Account.findOne({ where: { email } })
 
-    if (!match) {
-      throw new Error('Токен не верный')
-    }
+		if (!account) {
+			throw new Error('Пользователь с таким Email не найден')
+		}
 
-    const accessToken = createToken(account)
+		const match = await compare(
+			account.email + account.password + process.env.SECRET_JWT_KEY,
+			restoreHash
+		)
 
-    return accessToken
-  }
+		if (!match) {
+			throw new Error('Токен не верный')
+		}
 
-  // Отправка сообщения после регистрации
-  async _welcomeEmail(email, nik) {
-    const registryHash = await hash(
-      email + process.env.SECRET_JWT_KEY,
-      saltNumber
-    )
-    const link = `${process.env.APP_HOST}/accept/${email}/${registryHash}`
-    const message = compileFile('./views/emails/welkome.pug')({ nik, link })
-    mail(email, 'Регистрация на сайте Mafia One', message)
-  }
+		const accessToken = createToken(account)
 
-  // Отправка сообщения на почту со ссылкой для смены пароля
-  async _restoreEmail(account) {
-    const restoreHash = await hash(
-      account.email + account.password + process.env.SECRET_JWT_KEY,
-      saltNumber
-    )
-    const link = `${process.env.APP_HOST}/restore/${account.email}/${restoreHash}`
-    const message = compileFile('./views/emails/restore.pug')({ link })
-    mail(account.email, 'Восстановление пароля на Mafia One', message)
-  }
+		return accessToken
+	}
+
+	// Отправка сообщения после регистрации
+	async _welcomeEmail(email, nik) {
+		const registryHash = await hash(
+			email + process.env.SECRET_JWT_KEY,
+			saltNumber
+		)
+		const link = `${process.env.APP_HOST}/accept/${email}/${registryHash}`
+		const message = compileFile('./views/emails/welkome.pug')({ nik, link })
+		mail(email, 'Регистрация на сайте Mafia One', message)
+	}
+
+	// Отправка сообщения на почту со ссылкой для смены пароля
+	async _restoreEmail(account) {
+		const restoreHash = await hash(
+			account.email + account.password + process.env.SECRET_JWT_KEY,
+			saltNumber
+		)
+		const link = `${process.env.APP_HOST}/restore/${account.email}/${restoreHash}`
+		const message = compileFile('./views/emails/restore.pug')({ link })
+		mail(account.email, 'Восстановление пароля на Mafia One', message)
+	}
 }
 
 module.exports = new AuthService()
