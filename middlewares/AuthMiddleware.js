da0286df6706582d0ef56c65621d97520d871f9c
Vladimir <vzangi@yandex.ru>
2024-03-09 09:32:44 +0300

    fix punish weight

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -3,117 +3,92 @@ const Account = require('../models/Account')
 const Punishment = require('../models/Punishment')
 
 const isAuth = (req, res, next) => {
-  const { user } = req
-  if (user) return next()
-  res.redirect('/login')
+	const { user } = req
+	if (user) return next()
+	res.redirect('/login')
+}
+
+// Получение текущего пользователя
+const getCurrentAccount = async (user) => {
+	if (!user) return null
+
+	const account = await Account.findOne({
+		where: {
+			id: user.id,
+		},
+		attributes: [
+			'id',
+			'username',
+			'avatar',
+			'vipTo',
+			'online',
+			'vip',
+			'wallet',
+			'status',
+			'role',
+			'gender',
+			'rank',
+			'level',
+			'email',
+			'telegramChatId',
+		],
+		include: [
+			{
+				model: Punishment,
+				where: {
+					untilAt: {
+						[Op.gt]: new Date().toISOString(),
+					},
+				},
+				required: false,
+			},
+		],
+	})
+
+	if (!account) return null
+
+	const canLogin =
+		account.punishments.filter((p) => p.type == Punishment.types.NO_LOGIN)
+			.length == 0
+
+	if (!canLogin) return null
+
+	return account
 }
 
 // добавляю авторизованного пользователя в переменную user шаблонизатора
 const userToTemplate = async (req, res, next) => {
-  const { user } = req
+	const { user } = req
 
-  if (!user) {
-    return next()
-  }
+	const account = await getCurrentAccount(user)
 
-  const account = await Account.findOne({
-    where: {
-      id: user.id,
-    },
-    attributes: [
-      'id',
-      'username',
-      'avatar',
-      'vipTo',
-      'online',
-      'vip',
-      'wallet',
-      'status',
-      'role',
-      'gender',
-      'rank',
-      'level',
-      'email',
-      'telegramChatId',
-    ],
-    include: [
-      {
-        model: Punishment,
-        where: {
-          untilAt: {
-            [Op.gt]: new Date().toISOString(),
-          },
-        },
-        required: false,
-      },
-    ],
-  })
+	if (account) {
+		req.account = account
+		res.locals.currentAccount = account
+	} else {
+		delete req.user
+	}
 
-  const canLogin =
-    account.punishments.filter((p) => p.type == Punishment.types.NO_LOGIN)
-      .length == 0
-
-  if (canLogin) {
-    req.account = account
-    res.locals.currentAccount = account
-  } else {
-    delete req.user
-  }
-
-  next()
+	next()
 }
 
+// Добавляю пользователя в сокет
 const userToSocket = async (socket, next) => {
-  const { user } = socket
-  if (!user) return next()
+	const { user } = socket
 
-  const account = await Account.findOne({
-    where: {
-      id: user.id,
-    },
-    attributes: [
-      'id',
-      'username',
-      'avatar',
-      'vipTo',
-      'online',
-      'vip',
-      'wallet',
-      'status',
-      'role',
-      'gender',
-      'rank',
-      'level',
-      'email',
-    ],
-    include: [
-      {
-        model: Punishment,
-        where: {
-          untilAt: {
-            [Op.gt]: new Date().toISOString(),
-          },
-        },
-        required: false,
-      },
-    ],
-  })
+	const account = await getCurrentAccount(user)
 
-  const canLogin =
-    account.punishments.filter((p) => p.type == Punishment.types.NO_LOGIN)
-      .length == 0
+	if (account) {
+		socket.account = account
+	} else {
+		delete socket.user
+	}
 
-  if (canLogin) {
-    socket.account = account
-  } else {
-    delete socket.user
-  }
-
-  next()
+	next()
 }
 
 module.exports = {
-  isAuth,
-  userToTemplate,
-  userToSocket,
+	isAuth,
+	userToTemplate,
+	userToSocket,
 }
