46e084b9ce5ec691af6708099ab6b964c68d4ca5
Vladimir <vzangi@yandex.ru>
2024-03-12 16:08:38 +0300

    clear online flag on reboot

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
 const {
-	app, // express приложение
-	server, // http сервер
-	io, // сокеты
-	sequelize, // база данных
+  app, // express приложение
+  server, // http сервер
+  io, // сокеты
+  sequelize, // база данных
 } = require('./units/init')
 const GamesManager = require('./units/GamesManager')
 const log = require('./units/customLog')
+const Account = require('./models/Account')
 
 // Запуск сервера
 ;(async function () {
-	// Проверяю подключение к базе данных
-	sequelize
-		.authenticate()
-		.then(async () => {
-			log('DB connected... ok')
+  // Проверяю подключение к базе данных
+  sequelize
+    .authenticate()
+    .then(async () => {
+      log('DB connected... ok')
 
-			// Загружаю игры, которые были до старта сервера
-			await GamesManager.loadGames(io)
+      // Сбрасываю флаг онлайна
+      await Account.update(
+        {
+          online: false,
+        },
+        {
+          where: {
+            online: true,
+          },
+        }
+      )
 
-			// Подключаю обработку сокетов
-			require('./routes/SocketRouter')(io)
+      // Загружаю игры, которые были до старта сервера
+      await GamesManager.loadGames(io)
 
-			// Подключаю роутинг
-			require('./routes')(app)
+      // Подключаю обработку сокетов
+      require('./routes/SocketRouter')(io)
 
-			// Запускаю прослушку порта
-			const PORT = process.env.PORT || 3000
-			server.listen(PORT, () => {
-				log(`Server started on ${PORT} port`)
-			})
-		})
-		.catch((error) => {
-			log('Error on start: ' + error.message)
-		})
+      // Подключаю роутинг
+      require('./routes')(app)
+
+      // Запускаю прослушку порта
+      const PORT = process.env.PORT || 3000
+      server.listen(PORT, () => {
+        log(`Server started on ${PORT} port`)
+      })
+    })
+    .catch((error) => {
+      log('Error on start: ' + error.message)
+    })
 })()
