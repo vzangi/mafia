6e628f0dd725d3f9c8eebdca5cac41835c517418
Vladimir <vzangi@yandex.ru>
2024-03-15 13:23:03 +0300

    payment accept

--- services/YooKassaService.js
+++ services/YooKassaService.js
@@ -1,7 +1,49 @@
+const Notification = require('../models/Notification')
+const Payment = require('../models/Payment')
+const WalletEvent = require('../models/WalletEvents')
+
 class YooKassaService {
   // Пришёл ответ от ЮМоney на тестовую оплату
   async testResponse(data) {
     console.log(data)
+
+    if (!data) throw new Error('No data')
+
+    const { event, object } = data
+
+    if (!event || !object) throw new Error('No data')
+    if (event != 'payment.succeeded')
+      throw new Error(`Статус нотификации: ${event}`)
+
+    const { id, status } = object
+
+    if (!status || !id) throw new Error('No data')
+    if (status != 'succeeded') throw new Error(`Статус оплаты: ${status}`)
+
+    const payment = await Payment.findOne({ where: { pid: id } })
+
+    if (!payment) throw new Error('Платёж не найден')
+
+    if (payment.status == status)
+      throw new Error(`Платёж #${id} ранее уже был проведён`)
+
+    // Сохраняю статус платежа
+    payment.status = status
+    await payment.save()
+
+    const { accountId, amount } = payment
+
+    // Зачисляю средства на счёт
+    await WalletEvent.payment(accountId, amount)
+
+    const message = `Ваш кошелёк пополнен на ${amount} рублей`
+
+    // Отправляю нотификацию
+    const newNotify = await Notification.create({
+      accountId,
+      message,
+      level: 1,
+    })
   }
 }
 
