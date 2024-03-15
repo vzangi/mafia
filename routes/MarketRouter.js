069d74f9b530af1ce99f01f5591883edd38005e6
Vladimir <vzangi@yandex.ru>
2024-03-15 23:06:50 +0300

    seo updates

--- routes/MarketRouter.js
+++ routes/MarketRouter.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/MarketController')
+const { seoMiddelware } = require('../middlewares/SeoMiddleware')
+
+// Установка заголовков и описаний страниц
+router.use(seoMiddelware)
 
 // Маркет
 router.get('/', controller.market)
--- services/MarketService.js
+++ services/MarketService.js
@@ -7,103 +7,100 @@ const NaborThing = require('../models/NaborThing')
 const { Op } = require('sequelize')
 
 class MarketService {
-  // Список вещей на продажу
-  async marketData() {
-    const types = await ThingType.findAll({ order: [['sort']] })
-    const classes = await ThingClass.findAll({ order: [['sort']] })
-    const collections = await ThingCollection.findAll({ order: [['sort']] })
+	// Список вещей на продажу
+	async marketData() {
+		const types = await ThingType.findAll({ order: [['sort']] })
+		const classes = await ThingClass.findAll({ order: [['sort']] })
+		const collections = await ThingCollection.findAll({ order: [['sort']] })
 
-    const things = await AccountThing.getMarketList()
+		const things = await AccountThing.getMarketList()
 
-    const data = {
-      types,
-      classes,
-      collections,
-      things,
-      title: 'Маркет онлайн игры Mafia One',
-      description: 'Покупайте и продавайте вещи из своего арсенала',
-    }
-    return data
-  }
+		const data = {
+			types,
+			classes,
+			collections,
+			things,
+		}
+		return data
+	}
 
-  // Лоты игрока
-  async myLotsData(account) {
-    if (!account) {
-      throw new Error('Не авторизован')
-    }
-    const things = await AccountThing.findAll({
-      where: {
-        accountId: account.id,
-        marketPrice: {
-          [Op.ne]: null,
-        },
-      },
-      include: [
-        {
-          model: Thing,
-          include: [
-            { model: ThingClass },
-            { model: ThingCollection },
-            { model: ThingType },
-          ],
-        },
-      ],
-    })
+	// Лоты игрока
+	async myLotsData(account) {
+		if (!account) {
+			throw new Error('Не авторизован')
+		}
+		const things = await AccountThing.findAll({
+			where: {
+				accountId: account.id,
+				marketPrice: {
+					[Op.ne]: null,
+				},
+			},
+			include: [
+				{
+					model: Thing,
+					include: [
+						{ model: ThingClass },
+						{ model: ThingCollection },
+						{ model: ThingType },
+					],
+				},
+			],
+		})
 
-    const data = {
-      things,
-      title: `Мои лоты на маркете Mafia One`,
-    }
+		const data = {
+			things,
+		}
 
-    return data
-  }
+		return data
+	}
 
-  // Предложения по вещи
-  async thingData(thingId) {
-    if (!thingId) {
-      throw new Error('Нет необходимых данных')
-    }
+	// Предложения по вещи
+	async thingData(thingId) {
+		if (!thingId) {
+			throw new Error('Нет необходимых данных')
+		}
 
-    const thing = await Thing.findByPk(thingId, {
-      include: [
-        {
-          model: ThingClass,
-        },
-        {
-          model: ThingType,
-        },
-        {
-          model: ThingCollection,
-        },
-      ],
-    })
+		const thing = await Thing.findByPk(thingId, {
+			include: [
+				{
+					model: ThingClass,
+				},
+				{
+					model: ThingType,
+				},
+				{
+					model: ThingCollection,
+				},
+			],
+		})
 
-    if (!thing) {
-      throw new Error('Вещь не найдена')
-    }
+		if (!thing) {
+			throw new Error('Вещь не найдена')
+		}
 
-    const thingOffers = await AccountThing.getThingList(thingId)
+		const thingOffers = await AccountThing.getThingList(thingId)
 
-    const data = {
-      thing,
-      thingOffers,
-      title: thing.name + ' - покупайте в маркете игры Mafia One',
-      description: thing.description,
-    }
+		const data = {
+			thing,
+			thingOffers,
+			title: `Покупайте ${thing.name} на маркете онлайн игры Мафия`,
+			description: thing.description,
+		}
 
-    // Если вещь является набором или кейсом - подгружаю вещи входящие в него
-    if (thing.thingtypeId == 3 || thing.thingtypeId == 4) {
-      data.items = await NaborThing.findAll({
-        where: {
-          naborId: thing.id,
-        },
-        include: [{ model: Thing }],
-        order: [['thing', 'thingclassId']],
-      })
-    }
+		// Если вещь является набором или кейсом - подгружаю вещи входящие в него
+		if (thing.thingtypeId == 3 || thing.thingtypeId == 4) {
+			data.items = await NaborThing.findAll({
+				where: {
+					naborId: thing.id,
+				},
+				include: [{ model: Thing }],
+				order: [['thing', 'thingclassId']],
+			})
+		}
 
-    return data
-  }
+		return data
+	}
 }
 
 module.exports = new MarketService()
