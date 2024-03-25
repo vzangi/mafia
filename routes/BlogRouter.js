1e7877c30ce486cf8bb85f9a99d86fc55e32a85f
Vladimir <vzangi@yandex.ru>
2024-03-25 12:03:39 +0300

    add multimode

--- routes/BlogRouter.js
+++ routes/BlogRouter.js
@@ -1,23 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const pages = [
-	'bans',
-	'guidelines',
-	'how-to-play',
-	'inventory',
-	'payments',
-	'ranks',
-	'rules',
-	'vip',
-	'roles',
-	'classic',
-	'shootout',
+  'bans',
+  'guidelines',
+  'how-to-play',
+  'inventory',
+  'payments',
+  'ranks',
+  'rules',
+  'vip',
+  'roles',
+  'classic',
+  'shootout',
+  'multimode',
 ]
 
 pages.map((page) => {
-	router.get(`/${page}`, (req, res) => {
-		res.render(`pages/blog/${page}`)
-	})
+  router.get(`/${page}`, (req, res) => {
+    res.render(`pages/blog/${page}`)
+  })
 })
 
 module.exports = router
