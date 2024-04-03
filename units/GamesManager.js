1df3ea4b0b9f524dcc556e64c0a102684edf155d
Vladimir <vzangi@yandex.ru>
2024-04-03 20:02:51 +0300

    fix contructor 3 players for vip

--- units/GamesManager.js
+++ units/GamesManager.js
@@ -18,335 +18,336 @@ const GameInitRole = require('../models/GameInitRole')
     Класс для управления текущими играми
     ==================================== */
 class GamesManager {
-  // Буфер для хранения текущих игр
-  static activeGames = {}
+	// Буфер для хранения текущих игр
+	static activeGames = {}
 
-  // Буфер для заявок на игры
-  static whatingGames = {}
+	// Буфер для заявок на игры
+	static whatingGames = {}
 
-  /* =====================
+	/* =====================
      Загрузка игр и заявок
      ===================== */
-  static async loadGames(io) {
-    // Активные игры
-    const activeGames = await Game.scope('active').findAll()
+	static async loadGames(io) {
+		// Активные игры
+		const activeGames = await Game.scope('active').findAll()
 
-    // Загружаю воркеры для каждой игры
-    activeGames.forEach((game) => {
-      try {
-        GamesManager.start(io, game)
-      } catch (error) {
-        log(`Ошибка при запуске воркера заявки ${game.id}: ${error.message}`)
-      }
-    })
+		// Загружаю воркеры для каждой игры
+		activeGames.forEach((game) => {
+			try {
+				GamesManager.start(io, game)
+			} catch (error) {
+				log(`Ошибка при запуске воркера заявки ${game.id}: ${error.message}`)
+			}
+		})
 
-    // Заявки в лобби
-    const whatingGames = await Game.scope('whaiting').findAll()
+		// Заявки в лобби
+		const whatingGames = await Game.scope('whaiting').findAll()
 
-    // Загрузка заявок в буфер (с доступом по номеру заявки)
-    whatingGames.forEach((game) => (GamesManager.whatingGames[game.id] = game))
+		// Загрузка заявок в буфер (с доступом по номеру заявки)
+		whatingGames.forEach((game) => (GamesManager.whatingGames[game.id] = game))
 
-    // Запускаю интервал проверяющий дедлайны текущих заявок
-    setInterval(GamesManager.checkDeadLine.bind(null, io), deadlineInterval)
+		// Запускаю интервал проверяющий дедлайны текущих заявок
+		setInterval(GamesManager.checkDeadLine.bind(null, io), deadlineInterval)
 
-    // Запускаю интервал проверяющий завершённые игры
-    setInterval(GamesManager.checkEndedGames, 1000 * 60)
+		// Запускаю интервал проверяющий завершённые игры
+		setInterval(GamesManager.checkEndedGames, 1000 * 60)
 
-    // Запускаю интервал проверяющий очередь ждущих сорев
-    setInterval(GamesManager.checkContests.bind(null, io), 1000 * 5)
-  }
+		// Запускаю интервал проверяющий очередь ждущих сорев
+		setInterval(GamesManager.checkContests.bind(null, io), 1000 * 5)
+	}
 
-  /*  ====================================
+	/*  ====================================
       Проверка заявок на истекший дедлайн
       ==================================== */
-  static async checkDeadLine(io) {
-    // Беру текущее время
-    const date = new Date()
+	static async checkDeadLine(io) {
+		// Беру текущее время
+		const date = new Date()
 
-    // Прохожу в цикле по каждой заявке
-    for (let gameId in GamesManager.whatingGames) {
-      // Беру заявку
-      const game = GamesManager.whatingGames[gameId]
+		// Прохожу в цикле по каждой заявке
+		for (let gameId in GamesManager.whatingGames) {
+			// Беру заявку
+			const game = GamesManager.whatingGames[gameId]
 
-      // Если время дедлайна заявки меньше текущего времени
-      if (game.deadline < date) {
-        // Убираю заявку из списка ожидания
-        delete GamesManager.whatingGames[gameId]
+			// Если время дедлайна заявки меньше текущего времени
+			if (game.deadline < date) {
+				// Убираю заявку из списка ожидания
+				delete GamesManager.whatingGames[gameId]
 
-        // Проверяю, хватает ли игроков в заявке чтобы начать игру
-        const check = await GamesManager.checkNeededPlayersCount(game)
+				// Проверяю, хватает ли игроков в заявке чтобы начать игру
+				const check = await GamesManager.checkNeededPlayersCount(game)
 
-        // Если хватает
-        if (check) {
-          try {
-            // Пытаюсь запустить игру
-            await GamesManager.start(io, game)
-            break
-          } catch (error) {
-            log(error)
-          }
-        }
+				// Если хватает
+				if (check) {
+					try {
+						// Пытаюсь запустить игру
+						await GamesManager.start(io, game)
+						break
+					} catch (error) {
+						log(error)
+					}
+				}
 
-        // Если игроков не хватает или игру не удалось запустить - удаляю её
-        await GamesManager.remove(io, game)
-      }
-    }
-  }
+				// Если игроков не хватает или игру не удалось запустить - удаляю её
+				await GamesManager.remove(io, game)
+			}
+		}
+	}
 
-  /*  ======================================
+	/*  ======================================
       Исключение завершившихся игр из буфера
       ====================================== */
-  static checkEndedGames() {
-    for (let gameId in GamesManager.activeGames) {
-      const ag = GamesManager.activeGames[gameId]
-      if (ag.game.status == Game.statuses.ENDED) {
-        GamesManager.removeGame(gameId)
-      }
-    }
-  }
+	static checkEndedGames() {
+		for (let gameId in GamesManager.activeGames) {
+			const ag = GamesManager.activeGames[gameId]
+			if (ag.game.status == Game.statuses.ENDED) {
+				GamesManager.removeGame(gameId)
+			}
+		}
+	}
 
-  /*  ====================================
+	/*  ====================================
     Проверка очереди на сорев
     ==================================== */
-  static async checkContests(io) {
-    // Запрос спсика заявок с вышедшим временем ожидания
-    const deadWhere = {
-      where: {
-        createdAt: {
-          [Op.lt]: new Date(Date.now() - 1000 * 60 * ContestPlayer.whaitingTime)
-            .toISOString()
-            .replace('T', ' '),
-        },
-      },
-    }
+	static async checkContests(io) {
+		// Запрос спсика заявок с вышедшим временем ожидания
+		const deadWhere = {
+			where: {
+				createdAt: {
+					[Op.lt]: new Date(Date.now() - 1000 * 60 * ContestPlayer.whaitingTime)
+						.toISOString()
+						.replace('T', ' '),
+				},
+			},
+		}
 
-    //Ищу заявки у которых завершился дэдлайн
-    const deadlined = await ContestPlayer.findAll(deadWhere)
+		//Ищу заявки у которых завершился дэдлайн
+		const deadlined = await ContestPlayer.findAll(deadWhere)
 
-    // Если такие заявки найдены
-    if (deadlined.length != 0) {
-      // Удаляю их
-      await ContestPlayer.destroy(deadWhere)
+		// Если такие заявки найдены
+		if (deadlined.length != 0) {
+			// Удаляю их
+			await ContestPlayer.destroy(deadWhere)
 
-      // Обновляю количество ожидающих сорева
-      const concount = await ContestPlayer.concount()
-      io.of('/lobbi').emit('update.concount', concount)
-    }
+			// Обновляю количество ожидающих сорева
+			const concount = await ContestPlayer.concount()
+			io.of('/lobbi').emit('update.concount', concount)
+		}
 
-    // Беру одного из ждущих игроков
-    const contestPlayer = await ContestPlayer.findOne({
-      include: [{ model: Contest }],
-      order: sequelize.random(),
-    })
+		// Беру одного из ждущих игроков
+		const contestPlayer = await ContestPlayer.findOne({
+			include: [{ model: Contest }],
+			order: sequelize.random(),
+		})
 
-    // Если нет ожидающих игроков - выхожу
-    if (!contestPlayer) return
-    const { contest } = contestPlayer
+		// Если нет ожидающих игроков - выхожу
+		if (!contestPlayer) return
+		const { contest } = contestPlayer
 
-    // Необходимое количество игроков
-    const neededPlayersCount = contest.playersCount
+		// Необходимое количество игроков
+		const neededPlayersCount = contest.playersCount
 
-    // Игроки ждущие тот же режим
-    const players = await ContestPlayer.findAll({
-      where: {
-        contestId: contest.id,
-      },
-      include: [{ model: Account }],
-      limit: neededPlayersCount,
-      order: sequelize.random(),
-    })
+		// Игроки ждущие тот же режим
+		const players = await ContestPlayer.findAll({
+			where: {
+				contestId: contest.id,
+			},
+			include: [{ model: Account }],
+			limit: neededPlayersCount,
+			order: sequelize.random(),
+		})
 
-    // Если найдено необходимое количество игроков
-    if (neededPlayersCount == players.length) {
-      const gameData = {}
-      gameData.accountId = contestPlayer.accountId
-      gameData.gametypeId = contest.gametypeId
-      gameData.playersCount = contest.playersCount
-      gameData.waitingTime = 0
-      gameData.deadline = new Date().toISOString()
-      gameData.competition = true
-      gameData.mode = contest.mode
+		// Если найдено необходимое количество игроков
+		if (neededPlayersCount == players.length) {
+			const gameData = {}
+			gameData.accountId = contestPlayer.accountId
+			gameData.gametypeId = contest.gametypeId
+			gameData.playersCount = contest.playersCount
+			gameData.waitingTime = 0
+			gameData.deadline = new Date().toISOString()
+			gameData.competition = true
+			gameData.mode = contest.mode
 
-      // Создаю игру
-      const newGame = await Game.create(gameData)
-      const game = await Game.scope('whaiting').findByPk(newGame.id)
+			// Создаю игру
+			const newGame = await Game.create(gameData)
+			const game = await Game.scope('whaiting').findByPk(newGame.id)
 
-      // Добавляю в неё игроков
-      for (const index in players) {
-        // Беру аккаунт игрока
-        const { account } = players[index]
+			// Добавляю в неё игроков
+			for (const index in players) {
+				// Беру аккаунт игрока
+				const { account } = players[index]
 
-        // Добавляю его в партию
-        await GamePlayer.create({
-          gameId: game.id,
-          accountId: account.id,
-          username: account.username,
-        })
+				// Добавляю его в партию
+				await GamePlayer.create({
+					gameId: game.id,
+					accountId: account.id,
+					username: account.username,
+				})
 
-        // Удаляю его заявки из очереди
-        await ContestPlayer.destroy({
-          where: {
-            accountId: account.id,
-          },
-        })
-      }
+				// Удаляю его заявки из очереди
+				await ContestPlayer.destroy({
+					where: {
+						accountId: account.id,
+					},
+				})
+			}
 
-      // Обновляю количество ожидающих сорева
-      const concount = await ContestPlayer.concount()
-      io.of('/lobbi').emit('update.concount', concount)
+			// Обновляю количество ожидающих сорева
+			const concount = await ContestPlayer.concount()
+			io.of('/lobbi').emit('update.concount', concount)
 
-      // Помещаю игру в очередь для запуска
-      GamesManager.whatingGames[game.id] = game
-    }
-  }
+			// Помещаю игру в очередь для запуска
+			GamesManager.whatingGames[game.id] = game
+		}
+	}
 
-  /*  ==========================================================
+	/*  ==========================================================
       Проверка на необходимое количество игроков для начала игры
       ========================================================== */
-  static async checkNeededPlayersCount(game) {
-    // Беру игроков, которые находятся в заявке
-    const playersInGame = await GamePlayer.count({
-      where: {
-        gameId: game.id,
-        status: GamePlayer.playerStatuses.WHAITNG,
-      },
-    })
+	static async checkNeededPlayersCount(game) {
+		// Беру игроков, которые находятся в заявке
+		const playersInGame = await GamePlayer.count({
+			where: {
+				gameId: game.id,
+				status: GamePlayer.playerStatuses.WHAITNG,
+			},
+		})
 
-    if (playersInGame < 3) return false
+		if (playersInGame < 3) return false
 
-    // Если конструктор
-    if (game.gametypeId == Game.types.CONSTRUCTOR) {
-      // Игроков должно быть больше, чем задано ролей
-      const needCount = await GameInitRole.rolesCount(game.id)
-      if (needCount > playersInGame) return false
-    }
+		// Если конструктор
+		if (game.gametypeId == Game.types.CONSTRUCTOR) {
+			// Игроков должно быть больше, чем задано ролей
+			const needCount = await GameInitRole.rolesCount(game.id)
+			if (needCount > playersInGame) return false
+		}
 
-    // Админ может запускать любые игры
-    if (game.account && game.account.role == 1) return true
+		// Админ может запускать любые игры
+		if (game.account && game.account.role == 1) return true
 
-    // Если игроков больше или равно минимально допустимому количеству - возвращаю true
-    if (playersInGame >= minCount) return true
+		// Если игроков больше или равно минимально допустимому количеству - возвращаю true
+		if (playersInGame >= minCount || game.gametypeId == Game.types.CONSTRUCTOR)
+			return true
 
-    // Если предыдущие условия не сработали,
-    // значит игроков не хватает для начала игры
-    return false
-  }
+		// Если предыдущие условия не сработали,
+		// значит игроков не хватает для начала игры
+		return false
+	}
 
-  /*  ===========
+	/*  ===========
       Запуск игры
       =========== */
-  static async start(io, game) {
-    // Убираю игру из списка ожидающих заявок, если она ещё там
-    if (GamesManager.whatingGames[game.id]) {
-      delete GamesManager.whatingGames[game.id]
-    }
+	static async start(io, game) {
+		// Убираю игру из списка ожидающих заявок, если она ещё там
+		if (GamesManager.whatingGames[game.id]) {
+			delete GamesManager.whatingGames[game.id]
+		}
 
-    // Буфер для типа запускаемой игры
-    let gameTypeClass = null
+		// Буфер для типа запускаемой игры
+		let gameTypeClass = null
 
-    // Классический режим
-    if (game.gametype.id == 1) {
-      gameTypeClass = GameClassic
-    }
+		// Классический режим
+		if (game.gametype.id == 1) {
+			gameTypeClass = GameClassic
+		}
 
-    // Перестрелка
-    if (game.gametype.id == 2) {
-      gameTypeClass = GamePerestrelka
-    }
+		// Перестрелка
+		if (game.gametype.id == 2) {
+			gameTypeClass = GamePerestrelka
+		}
 
-    // Мультирежим
-    if (game.gametype.id == 4) {
-      gameTypeClass = GameMulti
-    }
+		// Мультирежим
+		if (game.gametype.id == 4) {
+			gameTypeClass = GameMulti
+		}
 
-    // Конструктор
-    if (game.gametype.id == 5) {
-      gameTypeClass = GameConstructor
-    }
+		// Конструктор
+		if (game.gametype.id == 5) {
+			gameTypeClass = GameConstructor
+		}
 
-    // Если тип игры не определён
-    if (!gameTypeClass) {
-      // Удаляю заявку
-      await GamesManager.remove(io, game)
+		// Если тип игры не определён
+		if (!gameTypeClass) {
+			// Удаляю заявку
+			await GamesManager.remove(io, game)
 
-      throw new Error('Игра этого типа пока не поддерживается')
-    }
+			throw new Error('Игра этого типа пока не поддерживается')
+		}
 
-    // Создаю воркер для новой игры
-    const newGame = new gameTypeClass(io, game)
+		// Создаю воркер для новой игры
+		const newGame = new gameTypeClass(io, game)
 
-    // Помещаю его в буфер текущих игр
-    GamesManager.activeGames[game.id] = newGame
+		// Помещаю его в буфер текущих игр
+		GamesManager.activeGames[game.id] = newGame
 
-    try {
-      // Запускаю процесс игры
-      await newGame.startGame()
-    } catch (error) {
-      log('Ошибка при запуске игры: ' + error.message)
+		try {
+			// Запускаю процесс игры
+			await newGame.startGame()
+		} catch (error) {
+			log('Ошибка при запуске игры: ' + error.message)
 
-      // Удаляю заявку
-      await GamesManager.remove(io, game)
-    }
-  }
+			// Удаляю заявку
+			await GamesManager.remove(io, game)
+		}
+	}
 
-  /*  ===============
+	/*  ===============
       Удаление заявки
       =============== */
-  static async remove(io, game) {
-    // Убираю игру из списка ожидающих заявок, если она ещё там
-    if (GamesManager.whatingGames[game.id]) {
-      delete GamesManager.whatingGames[game.id]
-    }
+	static async remove(io, game) {
+		// Убираю игру из списка ожидающих заявок, если она ещё там
+		if (GamesManager.whatingGames[game.id]) {
+			delete GamesManager.whatingGames[game.id]
+		}
 
-    // Ставлю статус - игра НЕ началась
-    game.status = Game.statuses.NOT_STARTED
-    await game.save()
+		// Ставлю статус - игра НЕ началась
+		game.status = Game.statuses.NOT_STARTED
+		await game.save()
 
-    // Освобождаю игроков из заявки
-    await GamePlayer.update(
-      {
-        status: GamePlayer.playerStatuses.LEAVE,
-      },
-      {
-        where: {
-          gameId: game.id,
-          status: [
-            GamePlayer.playerStatuses.WHAITNG,
-            GamePlayer.playerStatuses.IN_GAME,
-            GamePlayer.playerStatuses.FREEZED,
-          ],
-        },
-      }
-    )
+		// Освобождаю игроков из заявки
+		await GamePlayer.update(
+			{
+				status: GamePlayer.playerStatuses.LEAVE,
+			},
+			{
+				where: {
+					gameId: game.id,
+					status: [
+						GamePlayer.playerStatuses.WHAITNG,
+						GamePlayer.playerStatuses.IN_GAME,
+						GamePlayer.playerStatuses.FREEZED,
+					],
+				},
+			}
+		)
 
-    // Уведомляю игроков об удалении заявки
-    io.of('/lobbi').emit('game.remove', game.id)
-  }
+		// Уведомляю игроков об удалении заявки
+		io.of('/lobbi').emit('game.remove', game.id)
+	}
 
-  /*  ==================================
+	/*  ==================================
       Получение игры из буфера по номеру
       ================================== */
-  static getGame(gameId) {
-    try {
-      // Ищу игру в буфере по его номеру
-      const game = GamesManager.activeGames[gameId]
+	static getGame(gameId) {
+		try {
+			// Ищу игру в буфере по его номеру
+			const game = GamesManager.activeGames[gameId]
 
-      // Возвращаю игру
-      return game
-    } catch (error) {
-      log(`Игра ${gameId} не найдена`)
+			// Возвращаю игру
+			return game
+		} catch (error) {
+			log(`Игра ${gameId} не найдена`)
 
-      // Если игры нет в списке - возвращаю null
-      return null
-    }
-  }
+			// Если игры нет в списке - возвращаю null
+			return null
+		}
+	}
 
-  /*  ===================================
+	/*  ===================================
       Удаление игры из буфера текущих игр
       =================================== */
-  static removeGame(gameId) {
-    delete GamesManager.activeGames[gameId]
-  }
+	static removeGame(gameId) {
+		delete GamesManager.activeGames[gameId]
+	}
 }
 
 module.exports = GamesManager
