2d45818dda513e3c0ed43e994551d51b4681cfde
Vladimir <vzangi@yandex.ru>
2024-04-06 15:20:18 +0300

    double player in online fix

--- public/js/online.js
+++ public/js/online.js
@@ -1,26 +1,27 @@
 $(function () {
-  const filterInput = $('#filter')
+	const filterInput = $('#filter')
 
-  const onlineSocket = io('/online')
+	const onlineSocket = io('/online')
 
-  onlineSocket.on('online', (account) => {
-    $('#onlineUserTmpl').tmpl(account).prependTo('.users-list')
-    activateBSTooltips()
-  })
+	onlineSocket.on('online', (account) => {
+		if ($(`.friend-box[data-nik='${account.username}']`).length != 0) return
+		$('#onlineUserTmpl').tmpl(account).prependTo('.users-list')
+		activateBSTooltips()
+	})
 
-  onlineSocket.on('offline', (account) => {
-    const { username } = account
-    $(`[data-nik=${username}`).remove()
-  })
+	onlineSocket.on('offline', (account) => {
+		const { username } = account
+		$(`[data-nik=${username}`).remove()
+	})
 
-  filterInput.keyup(function () {
-    const filter = $(this).val()
-    $('.online-friend-box').each((_, friend) => {
-      if ($(friend).find('.friend-links a').text().indexOf(filter) < 0) {
-        $(friend).slideUp()
-      } else {
-        $(friend).slideDown()
-      }
-    })
-  })
+	filterInput.keyup(function () {
+		const filter = $(this).val()
+		$('.online-friend-box').each((_, friend) => {
+			if ($(friend).find('.friend-links a').text().indexOf(filter) < 0) {
+				$(friend).slideUp()
+			} else {
+				$(friend).slideDown()
+			}
+		})
+	})
 })
